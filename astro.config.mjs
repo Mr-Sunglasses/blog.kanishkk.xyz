f1ffe3b7e112ee7f84c536132411fb3feb17c0c6
Kanishk Pachauri <itskanishkp.py@gmail.com>
2026-05-23 05:46:22 +0530

    going to PROD

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog-kanishk.netlify.app/",
+  site: "https://blog.kanishkk.xyz/",
   base: "/",
   trailingSlash: "always",
   integrations: [
